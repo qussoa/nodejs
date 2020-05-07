5768db828959afe28e8c153180bd8c9657c9668c
qussoa <qussoa@naver.com>
2020-05-07 17:57:05 +0900

    2020-05-07 react 연습

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,54 +1,13 @@
 import React from "react";
-import axios from "axios";
-import Moive from "./Movies";
-import "./App.css";
-
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get(
-      "http://yts-proxy.now.sh/list_movies.json?sort_by=rating"
-    );
-    this.setState({ movies, isLoading: false });
-  };
-
-  componentDidMount() {
-    this.getMovies();
-  }
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader_text">Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map((movie) => (
-              <Moive
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </section>
-    );
-  }
+import Home from "./routes/Home";
+import About from "./routes/About";
+import { HashRouter, Route } from "react-router-dom";
+function App() {
+  return (
+    <HashRouter>
+      <Route path="/" component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+  );
 }
-
 export default App;
---
+++ movie-app/src/routes/About.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function About() {
+  return <span>방탄아 사랑해</span>;
+}
+export default About;
