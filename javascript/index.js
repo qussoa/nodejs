ade0ad118ee877282d3adb49fabc4722d2e6cd48
qussoa <qussoa@naver.com>
2020-05-12 17:50:55 +0900

    2020-05-12

---
+++ javascript/index.js
@@ -0,0 +1,11 @@
+const mon = "Mon";
+const tue = "Tue";
+const wed = "Wed";
+const thu = "Thu";
+const fri = "Fri";
+
+console.log(mon, tue, wed, thu, fri);
+
+const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fir", "Sat", "Sun"];
+
+console.log(daysOfWeek);
